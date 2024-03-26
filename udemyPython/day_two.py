2bcc403cd299414d1df5bdcd48701cbd1c907578
RandallKeur <108151926+RandallKeur7@users.noreply.github.com>
2024-03-26 16:10:05 -0400

    split entire main class into modules for each day for cleanliness

---
+++ udemyPython/day_two.py
@@ -0,0 +1,7 @@
+def tip_calculator():
+    print("Welcome to the tip calculator!")
+    total_bill = float(input("What was the total bill?: $"))
+    people = int(input("How many people to split the bill?: "))
+    tip_percent = int(input("What percentage tip would you like to give?: "))
+    tip = round((total_bill * (1 + tip_percent / 100)) / people, 2)
+    print(f"Each person should pay: ${tip}")
