a7caeb783a793cc82c89a041037e030ffec89bbe
RandallKeur <rkeur7@gmail.com>
2024-06-14 12:50:50 -0400

    add collect input function for second number

--- src/day_ten.py
+++ src/day_ten.py
@@ -4,22 +4,28 @@
 from src.ascii_art.art import CALCULATOR
 
 
+def collect_input():
+    return input("What\'s the next number?: ")
+
+
 def calculator():
     print(f"{CALCULATOR}")
     number_1 = input("What\'s the first number?: ")
     print(f"+\n-\n*\n/\n")
 
-    ##TODO loop here
-    operation = input("Pick an operation: ")
-    match operation:
-        case '+':
-            ## add
-        case '-':
-            subtract(number_1, number_2)
-        case '*':
-            multiply(number_1, number_2)
-        case '/':
-            divide(number_1, number_2)
-        case _:
-            print("Operation not valid")
-            valid_op = False
+    valid_op = False
+    while not valid_op:
+        operation = input("Pick an operation: ")
+        valid_op = True
+        match operation:
+            case '+':
+                number_1 += collect_input()
+            case '-':
+                number_1 -= collect_input()
+            case '*':
+                number_1 *= collect_input()
+            case '/':
+                number_1 /= collect_input()
+            case _:
+                print("Operation not valid\n")
+                valid_op = False
