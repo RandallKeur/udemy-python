c38d4d22d27bc8cf29e13102291ade2655a23af0
RandallsBagels <59258405+RandallKeur@users.noreply.github.com>
2024-06-17 10:04:27 -0400

    implement day 12, add test, fully test, and lint (#17)

--- src/constants/values.py
+++ src/constants/values.py
@@ -1,5 +1,6 @@
 """Class including constant values"""
 
+# CALCULATOR GAME
 CALCULATOR_OPERATIONS = '''
 +
 -
@@ -7,11 +8,14 @@ CALCULATOR_OPERATIONS = '''
 /
 '''
 
+# BLACKJACK GAME
 BLACKJACK_CARDS = {'1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9,
                    '10': 10, 'J': 10, 'Q': 10, 'K': 10, 'A': 11}
-
 HIT = 'hit'
-
 STAND = 'stand'
-
 BLACKJACK_21 = 21
+
+# NUMBER GUESSING GAME
+MIN = 1
+MAX = 100
+GUESSES = {'hard': 5, 'medium': 6, 'easy': 7}
---
+++ test/day_twelve_test.py
@@ -0,0 +1,31 @@
+"""Test for Day 12 of Coding Challenges"""
+import os
+import sys
+import unittest
+from io import StringIO
+from unittest import mock
+
+from src.day_twelve import number_guessing
+from src.constants.ascii_art import NUMBER_GUESS
+
+
+class DayTwelveTest(unittest.TestCase):
+    """Test for Day 12 of Coding"""
+
+    def test_number_guessing_art(self):
+        """Test the Number Guessing image"""
+        # setup
+        out = StringIO()
+        os.environ['TERM'] = 'xterm-256color'
+
+        with mock.patch('sys.stdin', new=StringIO('easy\n50\n25\n12\n6\n3\n2\n1')):
+            # given
+            art = NUMBER_GUESS.strip()
+            sys.stdout = out
+
+            # when
+            number_guessing()
+            actual = out.getvalue().strip()
+
+            # then
+            self.assertTrue(art in actual)
