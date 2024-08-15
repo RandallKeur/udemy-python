68398a2fd2b17f7a1369b7d575e343410ca25133
RandallsBagels <59258405+RandallKeur@users.noreply.github.com>
2024-08-14 20:43:29 -0400

    Chore/refactor coffee maker (#42)
    * create coffee shop class to encompass coffee activities without replacing existing implementation yet
    
    * appease linter and response types
    
    * remove test for day 16 and un-necessary files, need to write tests for class based challenges
    
    * quick correction and refactor for comment
    
    * reformat

---
+++ src/classes/coffee_shop/coffee_shop.py
@@ -0,0 +1,39 @@
+""" Coffee Shop"""
+from src.classes.coffee_shop.coffee_maker import CoffeeMaker
+from src.classes.coffee_shop.coin_converter import CoinConverter
+from src.classes.coffee_shop.menu import Menu
+
+
+class CoffeeShop:
+    """ Models the coffee shop"""
+    def __init__(self):
+        self.coffee_maker = CoffeeMaker()
+        self.menu = Menu()
+        self.coin_converter = CoinConverter()
+        self.open = True
+
+    def process_order(self, order: str) -> None:
+        """ Process the order from the customer"""
+        drink = self.menu.find_drink(order)
+        if drink is not None:
+            if (self.coffee_maker.is_resource_sufficient(drink) and
+                    self.coin_converter.make_payment(drink.cost)):
+                self.coffee_maker.make_coffee(drink)
+
+    def take_orders(self) -> None:
+        """ Takes order from the customer"""
+        while self.open:
+            options = self.menu.get_items()
+            order = input(f"What would you like?: ({options}): ")
+            if order == "report":
+                self.coffee_maker.report()
+                self.coin_converter.report()
+            elif order == "off":
+                self.close()
+            else:
+                self.process_order(order)
+
+    def close(self) -> None:
+        """ Closes the coffee shop"""
+        print("\nCoffee shop is closed, come see us tomorrow!\n")
+        self.open = False
